76dec57087fec1bf8a300b9b2c5db71ccf0c6e4e
Sean Nguyen <mdnsean@gmail.com>
2016-05-09 09:58:28 -0700

    trying to monkey patch mechanize error

--- app/helpers/scraper.rb
+++ app/helpers/scraper.rb
@@ -5,17 +5,21 @@ module Scraper
 		artist = artist.gsub(' ', '')
 		artist_url = artist[0] + '/' + artist + '.html'
 
-		page = agent.get('http://azlyrics.com/' + artist_url)
+		page = agent.get('https://azlyrics.com/' + artist_url)
 		song_links = page.links_with(href: /lyrics\/#{artist}/)
 		
-		song_links.each do |link|
-			parse_song(link)
-		end
+		# parse_song(song_links[0])
+
+		# song_links.each do |link|
+		# 	parse_song(link)
+		# end
 	end
 
 	def parse_song(link)
 		page = link.click
 		lyrics = page.css('div.ringtone~div')[0].children
+		
+		# parse_line(lyrics[0])
 		
 		# lyrics.each do |line|
 		# 	parse_line(line)
