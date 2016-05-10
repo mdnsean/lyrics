3e4e5632882d78dc63a92b566353b2dd0a0af547
Sean Nguyen <mdnsean@gmail.com>
2016-05-10 14:29:23 -0700

    cleaned up methods a little

--- app/helpers/scraper.rb
+++ app/helpers/scraper.rb
@@ -1,61 +1,45 @@
 module Scraper
 
-	def search_genius(artist)
-		agent = Mechanize.new
-		artist_url = artist.gsub(' ', '-')
+    def search_genius(artist)
+        agent = Mechanize.new
+        artist_url = artist.gsub(' ', '-')
 
-		page = agent.get('http://www.genius.com/artists/' + artist_url)
-		pagination_links = page.links_with(href: /for_artist_page/)
-		pagination_links.each do |link|
-			link.click
-		end
+        page = agent.get('http://www.genius.com/artists/' + artist_url)
+        pagination_links = page.links_with(href: /for_artist_page/)
+        pagination_links.each do |link|
+            link.click
+        end
 
-		song_links = page.links_with(href: /#{artist_url}.+lyrics/i)
-		
-		song = song_links[0].click
-		lyrics = song.css('p')[0].text.downcase.gsub(/\[.*\]/, '')
-		word_array = lyrics.split(/\s+/)
+        song_links = page.links_with(href: /#{artist_url}.+lyrics/i)
+        # only clicks first song (for now)
+        song = song_links[0].click
+        lyrics = song.css('p')[0].text.downcase.gsub(/\[.*\]/, '')
+        word_array = lyrics.split(/\s+/)
 
-		counts = {}
-		word_array.each do |word|
-			if counts[word]
-				counts[word] += 1
-			else
-				counts[word] = 1
-			end
-		end
-		puts counts
+        counts = {}
+        word_array.each do |word|
+            if counts[word]
+                counts[word] += 1
+            else
+                counts[word] = 1
+            end
+        end
+        puts counts
 
-	end
+    end
 
-	def search_azlyrics(artist)
-		agent = Mechanize.new
-		artist = artist.gsub(' ', '')
-		artist_url = artist[0] + '/' + artist + '.html'
+    def search_azlyrics(artist)
+        agent = Mechanize.new
+        artist = artist.gsub(' ', '')
+        artist_url = artist[0] + '/' + artist + '.html'
 
-		page = agent.get('http://azlyrics.com/' + artist_url)
-		song_links = page.links_with(href: /lyrics\/#{artist}/)
-		
-		# parse_song(song_links[0])
+        page = agent.get('http://azlyrics.com/' + artist_url)
+        song_links = page.links_with(href: /lyrics\/#{artist}/)
 
-		# song_links.each do |link|
-		# 	parse_song(link)
-		# end
-	end
+        song = song_links[0].click
+        lyrics = song.css('div.ringtone~div')[0].children
 
-	def parse_azlyrics_song(link)
-		page = link.click
-		lyrics = page.css('div.ringtone~div')[0].children
-		
-		# parse_line(lyrics[0])
-
-		# lyrics.each do |line|
-		# 	parse_line(line)
-		# end
-	end
-
-	def parse_line(line)
-		puts line
-	end
-
+        lyrics.each do |line|
+        end
+    end
 end
