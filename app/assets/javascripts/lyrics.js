5269af023cb458d6a3e1dcaac2e4c41a457058b7
Sean Nguyen <mdnsean@gmail.com>
2016-05-13 16:07:11 -0700

    example bar graph

--- app/assets/javascripts/lyrics.js
+++ app/assets/javascripts/lyrics.js
@@ -48,21 +48,24 @@ var code = (function() {
 
     // Bar chart
 
-    var data = [4, 8, 15, 16, 23, 42];
+    var barChart = function() {
+        var data = [4, 8, 15, 16, 23, 42];
 
-    var x = d3.scale.linear()
-        .domain([0, d3.max(data)])
-        .range([0, 420]);
+        var x = d3.scale.linear()
+            .domain([0, d3.max(data)])
+            .range([0, 420]);
 
-    d3.select(".chart")
-      .selectAll("div")
-        .data(data)
-      .enter().append("div")
-        .style("width", function(d) { return x(d) + "px"; })
-        .text(function(d) { return d; });
+        d3.select(".chart")
+          .selectAll("div")
+            .data(data)
+          .enter().append("div")
+            .style("width", function(d) { return x(d) + "px"; })
+            .text(function(d) { return d; });
+    };
 
     var start = function() {
         attachSelectArtistHandler();
+        barChart();
     };
 
     return {
