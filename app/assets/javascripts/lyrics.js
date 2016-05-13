e242763122b2e26567c5f47b485eaf8212bc5613
Sean Nguyen <mdnsean@gmail.com>
2016-05-13 15:41:02 -0700

    init bar chart tutorial

--- app/assets/javascripts/lyrics.js
+++ app/assets/javascripts/lyrics.js
@@ -46,6 +46,21 @@ var code = (function() {
         }
     };
 
+    // Bar chart
+
+    var data = [4, 8, 15, 16, 23, 42];
+
+    var x = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([0, 420]);
+
+    d3.select(".chart")
+      .selectAll("div")
+        .data(data)
+      .enter().append("div")
+        .style("width", function(d) { return x(d) + "px"; })
+        .text(function(d) { return d; });
+
     var start = function() {
         attachSelectArtistHandler();
     };
