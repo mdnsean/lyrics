515ece134a5b0f04d6d82b65cebefcc6d4a10a15
Sean Nguyen <mdnsean@gmail.com>
2016-05-17 11:08:15 -0700

    pseudocode for artist chart

--- app/assets/javascripts/lyrics.js
+++ app/assets/javascripts/lyrics.js
@@ -54,15 +54,25 @@ var code = (function() {
                 height: 200,
                 type: 'bar'
             },
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            title: {
+                text: 'Everyone\'s Favorite Words'
             },
-
+            xAxis: {
+                // categories: array of wc.words
+            },
+            yAxis: {
+                title: {
+                    text: 'Word Frequency'
+                }
+            },
             series: [{
-                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+                // name: get artist.name,
+                // data: array of wc.counts
+            }, {
+                name: 'Next Artist',
+                data: []
             }]
         });
-
         window.chart = new Highcharts.Chart({
             chart: {
                 renderTo: container2,
